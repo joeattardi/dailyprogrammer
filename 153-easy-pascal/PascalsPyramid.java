bc8e8ba8c5b341e244e649cd717bdbb7d7e4c8c9
Joe Attardi <Joseph_Attardi@dell.com>
2014-03-18 12:04:20 -0400

    Implemented using the trinomial expansion approach.

--- 153-easy-pascal/PascalsPyramid.java
+++ 153-easy-pascal/PascalsPyramid.java
@@ -1,31 +1,20 @@
-import java.math.BigInteger;
-
 public class PascalsPyramid {
     public static void main(String[] args) {
-        int size = Integer.parseInt(args[0]);
-        String[][] matrix = new String[size + 1][size + 1];
+        int level = Integer.parseInt(args[0]) - 1;
 
-        int digits = 2;
-
-        String resultStr = BigInteger.valueOf((long) (Math.pow(10, digits * (size + 1)) + Math.pow(10, digits) + 1))
-            .pow(size)
-            .toString();
-
-        int index = 1;
-        matrix[0][0] = resultStr.substring(0, 1);
-        for (int z = 1; z < matrix.length; z++) {
-            for (int y = 0; y < matrix.length; y++) {
-                matrix[z][y] = resultStr.substring(index, index + 2);
-                index += digits;
-            }
-        }
-
-        System.out.println(matrix[0][0]);
-        for (int row = 1; row < matrix.length; row++) {
-            for (int col = matrix.length - 1 - row; col < matrix.length; col++) {
-                System.out.print(Integer.parseInt(matrix[row][col]) + " ");
+        for (int row = 0; row <= level; row++) {
+            for (int col = 0; col <= row; col++) {
+                System.out.print(getDigit(row - col, level - row, col) + " ");
             }
             System.out.println();
         }
     }	
+
+    public static int getDigit(int a, int b, int c) {
+        return factorial(a + b + c) / (factorial(a)*factorial(b)*factorial(c));
+    }
+
+    public static int factorial(int n) {
+        return (n == 0) ? 1 : factorial(n - 1) * n;
+    }
 }
